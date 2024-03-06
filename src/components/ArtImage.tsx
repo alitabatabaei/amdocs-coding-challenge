c05a1b5430ab10022f2153d5b0f4ff649077518a
Ali Tabatabaei <tabatabaei.a@gmail.com>
2024-03-06 13:18:49 -0500

    fix ArtImage occasional failure due to image size

--- src/components/ArtImage.tsx
+++ src/components/ArtImage.tsx
@@ -2,7 +2,7 @@ import { Artwork } from '@/api';
 
 export default function ArtImage({ id, baseUrl, thumb }: Props) {
   if (!thumb) return null;
-  const width = Math.min(thumb.width, 6400);
+  const width = Math.min(thumb.width, 1200);
   const url = [baseUrl, '/', id, '/full/', width, ',/0/default.jpg'].join('');
   return <img src={url} width={width} alt={thumb.alt_text} className="rounded-md object-cover" />;
 }
